1b451a4d8e8c47b0005b6643ba76791b21de002b
José Antônio <85037819+dede6giu@users.noreply.github.com>
2024-10-22 14:39:49 -0300

    implementação e documentação

---
+++ include/Nome.h
@@ -0,0 +1,56 @@
+#ifndef NOME_H
+#define NOME_H
+
+#include <string>
+
+//! Classe Nome
+/*!
+  Classe Nome, string de m&aacute;ximo 30 caracteres.\n
+  232013031 - implementa&ccedil;&atilde;o, documenta&ccedil;&atilde;o.
+*/
+
+class Nome
+{
+    private:
+        //! Uma string.
+        /*! Armazena o valor do nome. */
+        std::string valor = "";
+
+        //! Nome::validar()
+        /*!
+          Verifica se o valor do nome est&aacute; dentro do limite de caracteres.
+          @return bool indicando sucesso da opera&ccedil;&atilde;o.
+        */
+        bool validar(std::string);
+
+    public:
+        //! Um inteiro p&uacute;blico
+        /*!
+          Armazena o tamanho m&aacute;ximo do valor.
+          @sa validar()
+        */
+        int const MAXLENGTH = 30;
+
+        //! Nome::setValor()
+        /*!
+          Armazena um novo valor dentro da classe.
+          @param string novo valor.
+          @return bool indicando sucesso da opera&ccedil;&atilde;o.
+          @sa getValor(), Nome::validar()
+        */
+        bool setValor(std::string);
+
+        //! Nome::getValor()
+        /*!
+          Fun&ccedil;&atilde;o para obter o valor da classe Nome.
+          @return O valor do nome
+        */
+        std::string getValor() const;
+};
+
+inline std::string Nome::getValor() const
+{
+    return valor;
+}
+
+#endif // NOME_H
