4cdf873b8199ddb99fecd028c3c7ac32f7e5715b
Gustavo Henrique <Gustavohgbzxc123@gmail.com>
2024-10-29 10:12:50 -0300

    documentação

--- src/Duracao.cpp
+++ src/Duracao.cpp
@@ -10,14 +10,6 @@ bool Duracao::setValor(int valor)
     return true;
 }
 
-//! Duracao::validar()
-/*!
-  Valida se o valor condiz com as condi&ccedil;&otilde;es de exist&ecirc;ncia.\n
-  O valor deve ser de 0 ao limite estabelecido dentro da classe (360)
-  @return bool indicando se o valor &eacute; v&aacute;lido ou n&atilde;o.
-*/
-
-
 bool Duracao::validar(int valor)
 {
     if (0 <= valor && valor <= MAXIMO)
