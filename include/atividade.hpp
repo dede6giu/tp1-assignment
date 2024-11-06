16a5965ef4e5787412ae8bb6ea1fbf3edd7c6348
marcinv07 <marciovds07@gmail.com>
2024-11-06 20:17:17 -0300

    Entidade Atividade

---
+++ include/atividade.hpp
@@ -0,0 +1,46 @@
+#ifndef ATIVIDADE_HPP_INCLUDED
+#define ATIVIDADE_HPP_INCLUDED
+
+//! entidade Atividade
+//! 232029274 - entidade Atividade
+using namespace std;
+
+class Atividade {
+
+private:
+    Codigo codigo;
+    Nome nome;
+    Data data;
+    Horario horario;
+    Duracao duracao;
+    Dinheiro preco;
+    Avaliacao avaliacao;
+
+public:
+
+    void setCodigo(const Codigo&);
+    Codigo getCodigo() const;
+
+    void setNome(const Nome&);
+    Nome getNome() const;
+
+    void setData(const Data&);
+    Data getData() const;
+
+    void setHorario(const Horario&);
+    Horario getHorario() const;
+
+    void setDuracao(const Duracao&);
+    Duracao getDuracao() const;
+
+    void setPreco(const Dinheiro&);
+    Dinheiro getPreco() const;
+
+    void setAvaliacao(const Avaliacao&);
+    Avaliacao getAvaliacao() const;
+};
+
+#endif // ATIVIDADE_HPP
+
+
+
