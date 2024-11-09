902486e8d13ffb4fe18f1f0060e586aee42fa18b
José Antônio <85037819+dede6giu@users.noreply.github.com>
2024-11-09 18:19:08 +0000

    identação e erro simples concertado

--- include/atividade.hpp
+++ include/atividade.hpp
@@ -6,41 +6,39 @@
     232029274 - implementação entidade Atividade
 */
 
-using namespace std;
-
 class Atividade {
 
-private:
-    Codigo codigo;
-    Nome nome;
-    Data data;
-    Horario horario;
-    Duracao duracao;
-    Dinheiro preco;
-    Avaliacao avaliacao;
-
-public:
-
-    void setCodigo(const Codigo&);
-    Codigo getCodigo() const;
-
-    void setNome(const Nome&);
-    Nome getNome() const;
-
-    void setData(const Data&);
-    Data getData() const;
-
-    void setHorario(const Horario&);
-    Horario getHorario() const;
-
-    void setDuracao(const Duracao&);
-    Duracao getDuracao() const;
-
-    void setPreco(const Dinheiro&);
-    Dinheiro getPreco() const;
-
-    void setAvaliacao(const Avaliacao&);
-    Avaliacao getAvaliacao() const;
+    private:
+        Codigo codigo;
+        Nome nome;
+        Data data;
+        Horario horario;
+        Duracao duracao;
+        Dinheiro preco;
+        Avaliacao avaliacao;
+    
+    public:
+    
+        void setCodigo(const Codigo&);
+        Codigo getCodigo() const;
+    
+        void setNome(const Nome&);
+        Nome getNome() const;
+    
+        void setData(const Data&);
+        Data getData() const;
+    
+        void setHorario(const Horario&);
+        Horario getHorario() const;
+    
+        void setDuracao(const Duracao&);
+        Duracao getDuracao() const;
+    
+        void setPreco(const Dinheiro&);
+        Dinheiro getPreco() const;
+    
+        void setAvaliacao(const Avaliacao&);
+        Avaliacao getAvaliacao() const;
 };
 
 #endif // ATIVIDADE_HPP
