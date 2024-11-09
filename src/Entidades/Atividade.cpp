512779689afd0b93bc4fe0aeb2934aafe23239a3
marcinv07 <145283448+marcinv07@users.noreply.github.com>
2024-11-09 16:57:14 -0300

    Update and rename src/atividade.cpp to src/Entidades/Atividade.cpp
    Alguns problemas para arrumar:
    
    Defina os métodos get como inline e dentro do .hpp, não no .cpp
    Mova ambos arquivos para uma pasta Entidades dentro do respectivo diretório.
    include/Entidades/Atividade.hpp
    src/Entidades/Atividade.cpp

--- src/Entidades/Atividade.cpp
+++ src/Entidades/Atividade.cpp
@@ -1,63 +1,33 @@
-#include "atividade.hpp"
-
+#include "Entidades/Atividade.hpp"
 //! entidade Atividade
-/*!
-    232029274 - implementacao entidade Atividade
-*/
+//! 232029274 - implementacao entidade Atividade
+
 
 void Atividade::setCodigo(const Codigo &novoCodigo) {
     this->codigo = novoCodigo;
-}
-
-Codigo Atividade::getCodigo() const {
-    return codigo;
 }
 
 void Atividade::setNome(const Nome &novoNome) {
     this->nome = novoNome;
 }
 
-Nome Atividade::getNome() const {
-    return nome;
-}
-
 void Atividade::setData(const Data &novaData) {
     this->data = novaData;
-}
-
-Data Atividade::getData() const {
-    return data;
 }
 
 void Atividade::setHorario(const Horario &novoHorario) {
     this->horario = novoHorario;
 }
 
-Horario Atividade::getHorario() const {
-    return horario;
-}
-
 void Atividade::setDuracao(const Duracao &novaDuracao) {
     this->duracao = novaDuracao;
-}
-
-Duracao Atividade::getDuracao() const {
-    return duracao;
 }
 
 void Atividade::setPreco(const Dinheiro &novoPreco) {
     this->preco = novoPreco;
 }
 
-Dinheiro Atividade::getPreco() const {
-    return preco;
-}
-
 void Atividade::setAvaliacao(const Avaliacao &novaAvaliacao) {
     this->avaliacao = novaAvaliacao;
-}
-
-Avaliacao Atividade::getAvaliacao() const {
-    return avaliacao;
 }
 
