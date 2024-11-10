e6128b291b378735515b05ab4da380a83becee3f
José Antônio <85037819+dede6giu@users.noreply.github.com>
2024-11-10 15:01:10 -0300

    documentação código

--- include/Dominios/Codigo.hpp
+++ include/Dominios/Codigo.hpp
@@ -6,22 +6,58 @@
 //! Classe Codigo
 /*!
   Classe Codigo, string de 6 caracteres alfanum&eacute;ricos\n
-  232013031 - implementa&ccdeil;&atilde;o, construtores
+  232013031 - implementa&ccdeil;&atilde;o, construtores, documentação
 */
 
 class Codigo
 {
     private:
+        //! Uma string.
+        /*! Armazena o valor do codigo. */
         std::string codigo = "";
+
+        //! Senha::validar()
+        /*!
+            Verifica se o tamanho da string fornecida é exatamente igual à CODELENGTH.
+            Também verifica se todos os caracteres são alfanuméricos sem diacríticos.
+            @param string à ser analisada.
+            @return bool indicando sucesso da operação.
+        */
         bool validar(std::string);
 
     public:
         static int const CODELENGTH = 6;
 
+        //! Construtor Vazio
+        /*!
+            Constrói um objeto Codigo com valor vazio.
+            @sa Codigo(std::string)
+        */
         Codigo();
+
+        //! Construtor Paramétrico
+        /*!
+            Recebe um valor string, o verifica e finalmente constrói um objeto
+            Codigo com o valor inserido.
+            @sa Codigo(), Codigo::validar()
+        */
         Codigo(std::string);
 
+        //! Codigo::setValor(std::string)
+        /*!
+            Recebe um valor string, o verifica e altera o valor atual do objeto.
+            @param string que alterará o valor codigo atual.
+            @return booleano indicando o sucesso da operação.
+            @sa codigo, Codigo::validar()
+        */
         bool setValor(std::string);
+
+        //! Codigo::getValor(std::string)
+        /*!
+            Retorna o valor atual do parâmetro codigo.
+            @return string codigo do objeto.
+            @sa codigo
+        */
         std::string getValor() const;
 };
 
