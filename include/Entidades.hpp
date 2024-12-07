fd46297acd0fcea78b912a899b0a274458012f60
José Antônio <85037819+dede6giu@users.noreply.github.com>
2024-12-07 16:55:24 -0300

    add in general hpp

---
+++ include/Entidades.hpp
@@ -0,0 +1,10 @@
+#ifndef ENTIDADES_HPP_INCLUDED
+#define ENTIDADES_HPP_INCLUDED
+
+#include "Entidades/Atividade.hpp"
+#include "Entidades/Conta.hpp"
+#include "Entidades/Destino.hpp"
+#include "Entidades/Hospedagem.hpp"
+#include "Entidades/Viagem.hpp"
+
+#endif // ENTIDADES_HPP_INCLUDED
