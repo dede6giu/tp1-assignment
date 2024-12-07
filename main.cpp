492285b29e3e5a5f0dc41a3c1ab4abdbd74014a7
José Antônio <85037819+dede6giu@users.noreply.github.com>
2024-12-07 18:11:13 -0300

    main para TU

--- main.cpp
+++ main.cpp
@@ -1,20 +1,134 @@
 #include <iostream>
-#include "TU/TUDuracao.hpp"
+#include <string>
+#include "TU.hpp"
 
 using namespace std;
 
 int main()
 {
-    TUDuracao testes;
+    cout << "Iniciando Testes de Unidade..." << endl;
 
-    switch (testes.run())
+    string suc = "SUCESSO";
+    string fal = "FALHA";
+
     {
-        case TUDuracao::SUCESSO:
-            cout << "SUCESSO" << endl;
-            break;
-        case TUDuracao::FALHA:
-            cout << "FALHA" << endl;
-            break;
+        TUAvaliacao* holder = new TUAvaliacao();
+        cout << "Avaliacao: ";
+        switch (holder->run())
+        {
+            case holder->SUCESSO:
+                cout << suc << endl;
+                break;
+            case holder->FALHA:
+                cout << fal << endl;
+                break;
+        }
+        delete holder;
+    }
+
+    {
+        TUCodigo* holder = new TUCodigo();
+        cout << "Codigo   : ";
+        switch (holder->run())
+        {
+            case holder->SUCESSO:
+                cout << suc << endl;
+                break;
+            case holder->FALHA:
+                cout << fal << endl;
+                break;
+        }
+        delete holder;
+    }
+
+    {
+        TUData* holder = new TUData();
+        cout << "Data     : ";
+        switch (holder->run())
+        {
+            case holder->SUCESSO:
+                cout << suc << endl;
+                break;
+            case holder->FALHA:
+                cout << fal << endl;
+                break;
+        }
+        delete holder;
+    }
+
+    {
+        TUDinheiro* holder = new TUDinheiro();
+        cout << "Dinheiro : ";
+        switch (holder->run())
+        {
+            case holder->SUCESSO:
+                cout << suc << endl;
+                break;
+            case holder->FALHA:
+                cout << fal << endl;
+                break;
+        }
+        delete holder;
+    }
+
+    {
+        TUDuracao* holder = new TUDuracao();
+        cout << "Duracao  : ";
+        switch (holder->run())
+        {
+            case holder->SUCESSO:
+                cout << suc << endl;
+                break;
+            case holder->FALHA:
+                cout << fal << endl;
+                break;
+        }
+        delete holder;
+    }
+
+    {
+        TUHorario* holder = new TUHorario();
+        cout << "Horario  : ";
+        switch (holder->run())
+        {
+            case holder->SUCESSO:
+                cout << suc << endl;
+                break;
+            case holder->FALHA:
+                cout << fal << endl;
+                break;
+        }
+        delete holder;
+    }
+
+    {
+        TUNome* holder = new TUNome();
+        cout << "Nome     : ";
+        switch (holder->run())
+        {
+            case holder->SUCESSO:
+                cout << suc << endl;
+                break;
+            case holder->FALHA:
+                cout << fal << endl;
+                break;
+        }
+        delete holder;
+    }
+
+    {
+        TUSenha* holder = new TUSenha();
+        cout << "Senha    : ";
+        switch (holder->run())
+        {
+            case holder->SUCESSO:
+                cout << suc << endl;
+                break;
+            case holder->FALHA:
+                cout << fal << endl;
+                break;
+        }
+        delete holder;
     }
 
     return 0;
