5f7cf7908e1002f51dc62efc0afc1ff073a482f7
Giovanni Daldegan <giovannidaldegani@gmail.com>
2024-12-08 15:37:20 -0300

    TUAtividade padronizada
    O protótipo dos testes mais completos tão no diretório principal do projeto, pra ficar guardado.

--- include/TU/TUAtividade.hpp
+++ include/TU/TUAtividade.hpp
@@ -4,40 +4,28 @@
 #include <vector>
 //#include
 #include "../../include/Entidades/Atividade.hpp"
+class TUAtividade
+{
+    private:
+        const std::string VALIDO_CODIGO = "iE4nmz";
+        const std::string VALIDO_NOME = "nome de teste";
+        const std::string VALIDO_DATA = "12/12/12";
+        const std::string VALIDO_HORARIO = "15:30";
+        const int VALIDO_DURACAO = 50;
+        const int VALIDO_PRECO = 200;
+        const int VALIDO_AVALIACAO = 3;
 
-class TUAtividade {
-private:
-    const std::vector<std::vector<std::string>> VALIDO = {
-        {"a124g2", "Cleber Oliveira", "07/12/24", "12:27", "27", "80", "2", "a124g2, Cleber Oliveira, 07/12/24, 12:27, 27, 80, 2"},
-        {"31453s", "Humpty Dumpty", "14/02/10", "03:43", "180", "250", "3", "31453s, Humpty Dumpty, 14/02/10, 03:43, 180, 250, 3"},
-        {"231gr3", "Hatsune Miku", "21/12/12", "17:30", "160", "400", "3", "231gr3, Hatsune Miku, 21/12/12, 17:30, 160, 400, 3"},
-        {"3lm153", "Marcos, o Assolador de Mundos", "31/12/99", "21:45", "2", "20", "4", "3lm153, Marcos, o Assolador de Mundos, 31/12/99, 21:45, 2, 20, 4"}
-    };
-    const std::vector<std::vector<std::string>> INVALIDO = {
-        {"das", "Joãozinho", "02/02/02", "02:02", "60", "207", "3", "das, Joãozinho, 02/02/02, 02:02, 60, 207, 3"},
-        {"qw1321", "FERNANDO ANTONIO DE ARAUJO CHACON DE ALBUQUERQUE", "60", "207", "3", "qw1321, FERNANDO ANTONIO DE ARAUJO CHACON DE ALBUQUERQUE, 60, 207, 3"},
-        {"qw1321", "Joãozinho", "28.12.01", "02:02", "60", "207", "3", "qw1321, Joãozinho, 28.12.01, 02:02, 60, 207, 3"},
-        {"qw1321", "Joãozinho", "29/02/15", "02:02", "60", "207", "3", "qw1321, Joãozinho, 29/02/15, 02:02, 60, 207, 3"},
-        {"qw1321", "Joãozinho", "31/11/22", "02:02", "60", "207", "3", "qw1321, Joãozinho, 31/11/22, 02:02, 60, 207, 3"},
-        {"qw1321", "Joãozinho", "02/02/02", "24:59", "60", "207", "3", "qw1321, Joãozinho, 02/02/02, 24:59, 60, 207, 3"},
-        {"qw1321", "Joãozinho", "02/02/02", "02:02", "380", "207", "3", "qw1321, Joãozinho, 02/02/02, 02:02, 380, 207, 3"},
-        {"qw1321", "Joãozinho", "02/02/02", "02:02", "60", "241335010", "3", "qw1321, Joãozinho, 02/02/02, 02:02, 60, 241335010, 3"},
-        {"qw1321", "Joãozinho", "02/02/02", "02:02", "60", "207", "7", "qw1321, Joãozinho, 02/02/02, 02:02, 60, 207, 7"},
-    };
+        Atividade *entidade;
+        int estado;
+        void setUp();
+        void tearDown();
+        void testarCenario();
 
-    Atividade *dominio;
-    int estado;
-    void setUp();
-    void tearDown();
-    void testarValido();
-    void testeUniVal(std::vector<std::string>);
-    void testarInvalido();
-    void testeUniInv(std::vector<std::string>);
-
-public:
-    const static int SUCESSO = 0;
-    const static int FALHA = -1;
-    int run();
+    public:
+        const static int SUCESSO = 0;
+        const static int FALHA = -1;
+        int run();
 };
+
 
 #endif // TUATIVIDADE_HPP_INCLUDED
--- src/TU/TUAtividade.cpp
+++ src/TU/TUAtividade.cpp
@@ -1,104 +1,59 @@
 #include <stdexcept>
 #include "../../include/TU/TUAtividade.hpp"
 
-using namespace std;
-
 void TUAtividade::setUp()
 {
-    dominio = new Atividade();
+    entidade = new Atividade();
     estado = SUCESSO;
 }
 
 void TUAtividade::tearDown()
 {
-    delete dominio;
+    delete entidade;
 }
 
-void TUAtividade::testeUniVal(vector<string> teste)
+void TUAtividade::testarCenario()
 {
-    Codigo codigo = Codigo(teste[0]);
-    Nome nome = Nome(teste[1]);
-    Data data = Data(); data.setValor(teste[2]);
-    Horario horario = Horario(); horario.setValor(teste[3]);
-    Duracao duracao = Duracao(stoi(teste[4]));
-    Dinheiro preco = Dinheiro(stoi(teste[5]));
-    Avaliacao avaliacao = Avaliacao(stoi(teste[6]));
+    Codigo codigo;
+    codigo.setValor(VALIDO_CODIGO);
+    entidade->setValor(codigo);
+    if (entidade->getCodigo().getValor() != VALIDO_CODIGO) estado = FALHA;
 
-    dominio->setValor(codigo);      // Codigo
-    dominio->setValor(nome);        // Nome
-    dominio->setValor(data);        // Data
-    dominio->setValor(horario);     // Horario
-    dominio->setValor(duracao);     // Duracao
-    dominio->setValor(preco);       // Dinheiro
-    dominio->setValor(avaliacao);   // Avaliacao
+    Nome nome;
+    nome.setValor(VALIDO_NOME);
+    entidade->setValor(nome);
+    if (entidade->getNome().getValor() != VALIDO_NOME) estado = FALHA;
 
-    if (dominio->getAtividade() != teste[7]) {
-        estado = FALHA;
-    }
-}
+    Data data;
+    data.setValor(VALIDO_DATA);
+    entidade->setValor(data);
+    if (entidade->getData().getValor() != VALIDO_DATA) estado = FALHA;
 
-void TUAtividade::testarValido()
-{
-    dominio = new Atividade();
+    Horario horario;
+    horario.setValor(VALIDO_HORARIO);
+    entidade->setValor(horario);
+    if (entidade->getHorario().getValor() != VALIDO_HORARIO) estado = FALHA;
 
-    try {
-        for (int i = 0; i < TUAtividade::VALIDO.size(); i++) {
-            testeUniVal(TUAtividade::VALIDO[i]);
-        }
-    }
-    catch(invalid_argument &excecao) {
-        estado = FALHA;
-    }
-    catch(length_error &excecao) {
-        estado = FALHA;
-    }
-}
+    Duracao duracao;
+    duracao.setValor(VALIDO_DURACAO);
+    entidade->setValor(duracao);
+    if (entidade->getDuracao().getValor() != VALIDO_DURACAO) estado = FALHA;
 
-void TUAtividade::testeUniInv(vector<string> teste)
-{
-    try
-    {
-        Codigo codigo = Codigo(teste[0]);
-        Nome nome = Nome(teste[1]);
-        Data data = Data(); data.setValor(teste[2]);
-        Horario horario = Horario(); horario.setValor(teste[3]);
-        Duracao duracao = Duracao(stoi(teste[4]));
-        Dinheiro preco = Dinheiro(stoi(teste[5]));
-        Avaliacao avaliacao = Avaliacao(stoi(teste[6]));
+    Dinheiro preco;
+    preco.setValor(VALIDO_PRECO);
+    entidade->setValor(preco);
+    if (entidade->getPreco().getValor() != VALIDO_PRECO) estado = FALHA;
 
-        dominio->setValor(codigo);      // Codigo
-        dominio->setValor(nome);        // Nome
-        dominio->setValor(data);        // Data
-        dominio->setValor(horario);     // Horario
-        dominio->setValor(duracao);     // Duracao
-        dominio->setValor(preco);       // Dinheiro
-        dominio->setValor(avaliacao);   // Avaliacao
-
-        estado = FALHA;
-    }
-    catch (invalid_argument &excecao) {
-        if (dominio->getAtividade() == teste[7])
-            estado = FALHA;
-
-    }
-    catch (length_error &excecao) {
-            if (dominio->getAtividade() == teste[7])
-                estado = FALHA;
-    }
-}
-
-void TUAtividade::testarInvalido()
-{
-    for (int i = 0; i < TUAtividade::INVALIDO.size(); i++) {
-        testeUniInv(TUAtividade::INVALIDO[i]);
-    }
+    Avaliacao avaliacao;
+    avaliacao.setValor(VALIDO_AVALIACAO);
+    entidade->setValor(avaliacao);
+    if (entidade->getAvaliacao().getValor() != VALIDO_AVALIACAO) estado = FALHA;
 }
 
 int TUAtividade::run()
 {
     setUp();
-    testarValido();
-    testarInvalido();
+    testarCenario();
     tearDown();
     return estado;
 }
