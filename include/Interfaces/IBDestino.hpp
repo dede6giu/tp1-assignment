e5b9eeb194876c3204885745b0a8e7c3a7ee13cc
José Antônio <85037819+dede6giu@users.noreply.github.com>
2025-02-01 13:04:34 +0000

    Update IBDestino.hpp

--- include/Interfaces/IBDestino.hpp
+++ include/Interfaces/IBDestino.hpp
@@ -9,9 +9,9 @@
 
 //! Interface Back Destino
 /*!
-    232002771 - Implementação e Documentação \n
+    232002771 - ImplementaÃ§Ã£o e DocumentaÃ§Ã£o \n
 
-    A IBDestino é responsável pela comunicação do usuário com
+    A IBDestino Ã© responsÃ¡vel pela comunicaÃ§Ã£o do usuÃ¡rio com
     o banco de dados quanto ao controle de Destino. Ela permite
     criar, excluir, ler e atualizar os dados de um Destino,
 */
@@ -22,60 +22,60 @@ class IBDestino
         //! Cria um Destino
         /*!
             Recebe os detalhes de um novo Destino e adiciona ao banco de dados.
-            Verifica se o Destino é único antes de realizar a operação.
+            Verifica se o Destino Ã© Ãºnico antes de realizar a operaÃ§Ã£o.
             @param Destino a ser criado.
-            @return Sucesso da operação.
+            @return Sucesso da operaÃ§Ã£o.
         */
         virtual bool criar(Destino) = 0;
 
         //! Cria uma Tabela
         /*!
             Recebe um Codigo e adiciona uma nova tabela com tal Codigo ao banco
-            de dados, se não já existir a tabela.
+            de dados, se nÃ£o jÃ¡ existir a tabela.
             @param Codigo da Conta.
         */
         virtual void criar(Codigo) = 0;
 
         //! Exclui um Destino
         /*!
-            Recebe um Destino e raliza uma confirmação antes da exclusão. Se o
-            Destino existe, ele é removido do banco de dados. Caso
-            contrário, a operação falha.
-            @param Destino a ser excluído.
-            @return Sucesso da operação.
+            Recebe um Destino e raliza uma confirmaÃ§Ã£o antes da exclusÃ£o. Se o
+            Destino existe, ele Ã© removido do banco de dados. Caso
+            contrÃ¡rio, a operaÃ§Ã£o falha.
+            @param Destino a ser excluÃ­do.
+            @return Sucesso da operaÃ§Ã£o.
         */
         virtual bool excluir(Destino) = 0;
 
         //! Exclui uma Tabela
         /*!
-            Recebe um Codigo e exclui a tabela associada ao Codigo. Método só deve
-            ser usado durante a exclusão de Conta.
-            @param Codigo da Conta excluída.
+            Recebe um Codigo e exclui a tabela associada ao Codigo. MÃ©todo sÃ³ deve
+            ser usado durante a exclusÃ£o de Conta.
+            @param Codigo da Conta excluÃ­da.
         */
         virtual void excluir(Codigo) = 0;
 
         //! Exclui Destinos
         /*!
-            Recebe dois Codigo. O primeiro é uma Conta, o segundo uma Viagem. Exclui
-            da Conta associada todos os Destino relacionados ao Viagem. Método só
-            deve ser acionado durante a exclusão de Viagem.
+            Recebe dois Codigo. O primeiro Ã© uma Conta, o segundo uma Viagem. Exclui
+            da Conta associada todos os Destino relacionados ao Viagem. MÃ©todo sÃ³
+            deve ser acionado durante a exclusÃ£o de Viagem.
             @param Codigo da Conta.
-            @param Codigo da Viagem excluída.
+            @param Codigo da Viagem excluÃ­da.
         */
         virtual void excluir(Codigo, Codigo) = 0;
 
-        //! Lê um Destino
+        //! LÃª um Destino
         /*!
-            Verifica a existência de um Destino no banco de dados.
+            Verifica a existÃªncia de um Destino no banco de dados.
             @param Destino a verificar.
-            @return Existência do Destino.
+            @return ExistÃªncia do Destino.
         */
         virtual bool ler(Destino) = 0;
 
-        //! Lê todos Destino
+        //! LÃª todos Destino
         /*!
-            Recebe dois Codigo. O primeiro é uma Conta, o segundo uma Viagem. Busca
-            da Conta associada por todos Destino relacionados ao Viagem.
+            Recebe dois Codigo. O primeiro Ã© uma Conta, o segundo uma Viagem. Busca
+            da Conta associada por todos Destino relacionados Ã  Viagem.
             @param Codigo da Conta.
             @param Codigo da Viagem.
             @return Retorna um vetor de Destino com todos os Destino cadastrados
@@ -87,18 +87,18 @@ class IBDestino
         /*!
             Atualiza o nome de um Destino.
             @param Destino a ser atualizado.
-            @param Novo Nome a ser atribuído ao Destino.
-            @return Sucesso da operação.
+            @param Novo Nome a ser atribuÃ­do ao Destino.
+            @return Sucesso da operaÃ§Ã£o.
         */
         virtual bool atualizar(Destino, Nome) = 0;
 
         //! Atualiza a Data de um Destino
         /*!
-            Atualiza a Data de início ou fim de um Destino.
+            Atualiza a Data de inÃ­cio ou fim de um Destino.
             @param Destino a ser atualizado.
-            @param Nova Data a ser atribuída ao Destino.
-            @param Bool false se for para atualizar início, true para fim.
-            @return Sucesso da operação.
+            @param Nova Data a ser atribuÃ­da ao Destino.
+            @param Bool false se for para atualizar inÃ­cio, true para fim.
+            @return Sucesso da operaÃ§Ã£o.
         */
         virtual bool atualizar(Destino, Data, bool) = 0;
 
@@ -106,24 +106,24 @@ class IBDestino
         /*!
             Atualiza a Avaliacao de um Destino.
             @param Destino a ser atualizada.
-            @param Nova Avaliacao a ser atribuída ao Destino.
-            @return Sucesso da operação.
+            @param Nova Avaliacao a ser atribuÃ­da ao Destino.
+            @return Sucesso da operaÃ§Ã£o.
         */
         virtual bool atualizar(Destino, Avaliacao) = 0;
 
-        //! Dependência da IBAtividade
+        //! DependÃªncia da IBAtividade
         /*!
-            Estabelece uma referência para a IBAtividade como uma variável, permitindo
-            acesso ao banco de dados pelos métodos da IBAtividade.
-            @param Referência à IBAtividade a ser salva.
+            Estabelece uma referÃªncia para a IBAtividade como uma variÃ¡vel, permitindo
+            acesso ao banco de dados pelos mÃ©todos da IBAtividade.
+            @param ReferÃªncia Ã  IBAtividade a ser salva.
         */
         virtual void setCntrIBAtividade(IBAtividade*) = 0;
 
-        //! Dependência da IBHospedagem
+        //! DependÃªncia da IBHospedagem
         /*!
-            Estabelece uma referência para a IBHospedagem como uma variável, permitindo
-            acesso ao banco de dados pelos métodos da IBHospedagem.
-            @param Referência à IBHospedagem a ser salva.
+            Estabelece uma referÃªncia para a IBHospedagem como uma variÃ¡vel, permitindo
+            acesso ao banco de dados pelos mÃ©todos da IBHospedagem.
+            @param ReferÃªncia Ã  IBHospedagem a ser salva.
         */
         virtual void setCntrIBHospedagem(IBHospedagem*) = 0;
 
